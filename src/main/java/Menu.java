822ffd4da8fa3c65b3fac15ea9fcc107a147e20c
Jakub112358 <jakubszymanski@hotmail.com>
2023-01-09 19:38:29 +0100

    refactored Menu class

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,54 +1,81 @@
 import java.util.Scanner;
 
 public class Menu {
+    private boolean running = true;
+
     public void startMenu() {
-        boolean running = true;
-        String input;
-        Scanner scanner = new Scanner(System.in);
-        while(running){
-            System.out.println("""
-                    Select:
-                    1 - add movie
-                    2 - display movies
-                    3 - terminate program
-                    """);
-            input = scanner.nextLine();
+        do {
+            menuAction();
+        } while (running);
+    }
 
-            switch (input){
-                case "1":
-                    System.out.println(" you've chosen adding movie");
-                    System.out.println("set title");
-                    String title = scanner.nextLine();
-                    System.out.println("set year of production");
-                    int yearOfProduction = scanner.nextInt();
-                    System.out.println("set genre");
-                    scanner.nextLine();
-                    String genre = scanner.nextLine();
-                    System.out.println("set rate");
-                    int score = scanner.nextInt();
-                    scanner.nextLine();
-                    if (MovieValidator.validateMovie(new Movie(title,yearOfProduction,genre,score))){
-                        MoviesData.getInstance().addMovie(new Movie(title,yearOfProduction,genre,score));
-                    } else {
-                        System.out.println("wrong input");
-                    }
-                    break;
-                case "2":
-                    System.out.println("you've chosen displaying movies");
-                    MoviesData.getInstance().getMovies();
-                    break;
-                case "3":
-                    System.out.println("terminating program");
-                    running = false;
-                    break;
-                default:
-                    System.out.println("wrong input, try again");
-
-            }
-        }
+    private void menuAction() {
+        showOptions();
+        String input = readDecision();
+        executeOption(input);
 
     }
 
 
+    private void executeOption(String input) {
 
+        switch (input) {
+            case "1":
+                System.out.println(" you've chosen adding movie");
+                Movie inputMovie = setNewMovie();
+                if (MovieValidator.validateMovie(inputMovie)) {
+                    MoviesData.getInstance().addMovie(inputMovie);
+                } else {
+                    System.out.println("wrong movie input");
+                }
+
+                break;
+            case "2":
+                System.out.println("you've chosen displaying movies");
+                MoviesData.getInstance().getMovies();
+                break;
+            case "3":
+                System.out.println("terminating program");
+                running = false;
+                break;
+            default:
+                System.out.println("wrong input, try again");
+
+        }
+    }
+
+    private Movie setNewMovie(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("set title");
+        String title = scanner.nextLine();
+        System.out.println("set year of production");
+        int yearOfProduction = scanner.nextInt();
+        System.out.println("set genre");
+        scanner.nextLine();
+        String genre = scanner.nextLine();
+        System.out.println("set rate");
+        int score = scanner.nextInt();
+        scanner.nextLine();
+        return new Movie(title, yearOfProduction, genre, score);
+    }
+
+
+    private void showOptions() {
+        System.out.println("""
+                Select:
+                1 - add movie
+                2 - display movies
+                3 - terminate program
+                """);
+    }
+
+    private String readDecision() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+
+    }
 }
+
+
+
+
